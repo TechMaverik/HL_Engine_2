18ef774fef6196fab18cdd438e0b117fd1722dc4
akhiljacob-trenser <akhil.jacob@trenser.com>
2023-07-25 11:50:12 +0530

    dataplotting prep for test

--- HL_Engine_Data_Visualization.py
+++ HL_Engine_Data_Visualization.py
@@ -12,28 +12,37 @@ class DataVisualizationEngine:
 
     def plot_line_graph(x,y,name,ycontent,xcontent):
         """line plotting"""
-        plt.plot(x,y)#[1,2],[3,4]
-        plt.title(name)
-        plt.ylabel(ycontent)
-        plt.xlabel(xcontent)
-        plt.show()
+        try:
+            plt.plot(x,y)#[1,2],[3,4]
+            plt.title(name)
+            plt.ylabel(ycontent)
+            plt.xlabel(xcontent)
+            plt.show()           
+        except:
+            return False
 
     def plot_histogram(x1,y1,x2,y2,label1,width1,label2,width2,xcontent,ycontent,heading):
         """histogram plotting"""
-        plt.bar(x1,y1,label=label1,width=width1)
-        plt.bar(x2,y2,label=label2,width=width2)
-        plt.legend()
-        plt.xlabel(xcontent)
-        plt.ylabel(ycontent)
-        plt.title(heading)
-        plt.show()
+        try:
+            plt.bar(x1,y1,label=label1,width=width1)
+            plt.bar(x2,y2,label=label2,width=width2)
+            plt.legend()
+            plt.xlabel(xcontent)
+            plt.ylabel(ycontent)
+            plt.title(heading)
+            plt.show()
+        except:
+            return False
 
     def plot_scatter(x,y,x1,y1,label1,label2,color1,color2,xlab,ylab,heading): 
         """scatter plotting"""
-        plt.scatter(x,y, label=label1,color=color1)
-        plt.scatter(x1,y1,label=label2,color=color2)
-        plt.xlabel(label1)
-        plt.ylabel(label2)
-        plt.title(heading)
-        plt.legend()
-        plt.show()
+        try:
+            plt.scatter(x,y, label=label1,color=color1)
+            plt.scatter(x1,y1,label=label2,color=color2)
+            plt.xlabel(label1)
+            plt.ylabel(label2)
+            plt.title(heading)
+            plt.legend()
+            plt.show()
+        except:
+            return False
